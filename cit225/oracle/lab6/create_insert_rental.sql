3cd1b5b1ef348449257981c0a29023b3d71e63fd
Zach Caldwell <zach.caldwell101@gmail.com>
2019-10-27 01:25:34 -0600

    [cit225] Lab 6 - fix insert_rental user id overrides

--- cit225/oracle/lab6/create_insert_rental.sql
+++ cit225/oracle/lab6/create_insert_rental.sql
@@ -19,19 +19,31 @@ CREATE OR REPLACE PROCEDURE insert_rental
 , pv_item_id             NUMBER
 , pv_return_date         DATE
 , pv_check_out_date      DATE     := TRUNC(SYSDATE)
-, pv_created_by          NUMBER   := ( SELECT   system_user_id
-                                       FROM     system_user
-                                       WHERE    system_user_name = 'SYSADMIN')
+, pv_created_by          NUMBER   := NULL
 , pv_creation_date       DATE     := SYSDATE
-, pv_last_updated_by     NUMBER   := ( SELECT   system_user_id
-                                       FROM     system_user
-                                       WHERE    system_user_name = 'SYSADMIN')
+, pv_last_updated_by     NUMBER   := NULL
 , pv_last_update_date    DATE     := SYSDATE) IS
-
+id_created_by            NUMBER   := pv_created_by;
+id_last_updated_by       NUMBER   := pv_last_updated_by;
 BEGIN
  
   /* Create a SAVEPOINT as a starting point. */
   SAVEPOINT starting_point;
+
+  -- Default to sysadmin if no user ids given
+  IF pv_created_by IS NULL THEN
+    SELECT   system_user_id
+    INTO     id_created_by
+    FROM     system_user
+    WHERE    system_user_name = 'SYSADMIN';
+  END IF;
+
+  IF pv_last_updated_by IS NULL THEN
+    SELECT   system_user_id
+    INTO     id_last_updated_by
+    FROM     system_user
+    WHERE    system_user_name = 'SYSADMIN';
+  END IF;
 
   /* Insert into the rental table. */
   INSERT INTO rental
@@ -40,9 +52,9 @@ BEGIN
   , pv_customer_id
   , pv_check_out_date
   , pv_return_date
-  , pv_created_by
+  , id_created_by
   , pv_creation_date
-  , pv_last_updated_by
+  , id_last_updated_by
   , pv_last_update_date );
 
   /* Insert into the rental_item table. */
@@ -51,9 +63,9 @@ BEGIN
   ( rental_item_s1.nextval
   , rental_s1.currval
   , pv_item_id
-  , pv_created_by
+  , id_created_by
   , pv_creation_date
-  , pv_last_updated_by
+  , id_last_updated_by
   , pv_last_update_date );
 
   /* Commit the series of inserts. */
