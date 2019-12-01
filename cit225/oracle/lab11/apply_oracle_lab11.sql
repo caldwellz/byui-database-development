227f0533ebd0c1dd19d9c3b6541de88a98bae6b3
Zach Caldwell <zach.caldwell101@gmail.com>
2019-11-30 17:23:50 -0700

    [cit225] Lab 11: Copy over Lab 10 queries

--- cit225/oracle/lab11/apply_oracle_lab11.sql
+++ cit225/oracle/lab11/apply_oracle_lab11.sql
@@ -34,7 +34,25 @@ FROM     rental;
 
 -- Merge transaction data into rental table.
 MERGE INTO rental target
-USING ( ... query_statement ... ) source
+USING (SELECT   DISTINCT
+                  r.rental_id
+         ,        c.contact_id
+         ,        tu.check_out_date AS check_out_date
+         ,        tu.return_date AS return_date
+         ,        1001 AS created_by
+         ,        TRUNC(SYSDATE) AS creation_date
+         ,        1001 AS last_updated_by
+         ,        TRUNC(SYSDATE) AS last_update_date
+         FROM   member m INNER JOIN transaction_upload tu
+         ON     m.account_number = tu.account_number
+         INNER JOIN contact c
+         ON     m.member_id = c.member_id LEFT JOIN rental r
+         ON     c.contact_id = r.customer_id
+         AND TRUNC(tu.check_out_date) = TRUNC(r.check_out_date)
+         AND TRUNC(tu.return_date) = TRUNC(r.return_date)
+         WHERE  c.first_name = tu.first_name
+         AND    NVL(c.middle_name,'x') = NVL(tu.middle_name,'x')
+         AND    c.last_name = tu.last_name) source
 ON (target.rental_id = source.rental_id)
 WHEN MATCHED THEN
 UPDATE SET last_updated_by = source.last_updated_by
@@ -42,17 +60,13 @@ UPDATE SET last_updated_by = source.last_updated_by
 WHEN NOT MATCHED THEN
 INSERT VALUES
 ( rental_s1.NEXTVAL
-, source.column_name
-...
-, source.column_name);
-
-
-
-
-
-
-
-
+, source.contact_id
+, source.check_out_date
+, source.return_date
+, source.created_by
+, source.creation_date
+, source.last_updated_by
+, source.last_update_date);
 
 
 -- Count rentals after insert.
@@ -69,7 +83,38 @@ FROM     rental_item;
 
 -- Merge transaction data into rental_item table.
 MERGE INTO rental_item target
-USING ( ... query_statement ... ) source
+USING (SELECT   r.rental_id
+       ,        tu.item_id
+       ,        1001 AS created_by
+       ,        TRUNC(SYSDATE) AS creation_date
+       ,        1001 AS last_updated_by
+       ,        TRUNC(SYSDATE) AS last_update_date
+       ,        cl.common_lookup_id AS rental_item_type
+       ,        p.amount AS rental_item_price
+       FROM member m INNER JOIN transaction_upload tu
+       ON     m.account_number = tu.account_number
+       INNER JOIN contact c
+       ON     m.member_id = c.member_id LEFT JOIN rental r
+       ON     c.contact_id = r.customer_id
+       AND TRUNC(tu.check_out_date) = TRUNC(r.check_out_date)
+       AND TRUNC(tu.return_date) = TRUNC(r.return_date)
+       LEFT JOIN common_lookup cl
+       ON cl.common_lookup_table = 'RENTAL_ITEM'
+       AND cl.common_lookup_column = 'RENTAL_ITEM_TYPE'
+       AND tu.rental_item_type = cl.common_lookup_type
+       LEFT JOIN rental_item ri
+       ON r.rental_id = ri.rental_id
+       LEFT JOIN common_lookup cl2
+       ON cl2.common_lookup_table = 'PRICE'
+       AND cl2.common_lookup_column = 'PRICE_TYPE'
+       AND cl2.common_lookup_code = cl.common_lookup_code
+       LEFT JOIN price p
+       ON p.item_id = tu.item_id
+       AND p.price_type = cl2.common_lookup_id
+       AND p.active_flag = 'Y'
+       WHERE  c.first_name = tu.first_name
+       AND    NVL(c.middle_name,'x') = NVL(tu.middle_name,'x')
+       AND    c.last_name = tu.last_name) source
 ON (target.rental_item_id = source.rental_item_id)
 WHEN MATCHED THEN
 UPDATE SET last_updated_by = source.last_updated_by
@@ -77,17 +122,14 @@ UPDATE SET last_updated_by = source.last_updated_by
 WHEN NOT MATCHED THEN
 INSERT VALUES
 ( rental_item_s1.nextval
-, source.column_name
-...
-, source.column_name);
-
-
-
-
-
-
-
-
+, source.rental_id
+, source.item_id
+, source.created_by
+, source.creation_date
+, source.last_updated_by
+, source.last_update_date
+, source.rental_item_type
+, source.rental_item_price);
 
 
 -- Count rental items after insert.
@@ -104,7 +146,70 @@ FROM     transaction;
 
 -- Merge transaction data into transaction table.
 MERGE INTO transaction target
-USING ( ... query_statement ... ) source
+USING (SELECT   t.transaction_id
+,        tu.payment_account_number AS transaction_account
+,        cl4.common_lookup_id AS transaction_type
+,        TRUNC(tu.transaction_date) AS transaction_date
+,       (SUM(tu.transaction_amount) / 1.06) AS transaction_amount
+,        r.rental_id
+,        cl3.common_lookup_id AS payment_method_type
+,        m.credit_card_number AS payment_account_number
+,        1001 AS created_by
+,        TRUNC(SYSDATE) AS creation_date
+,        1001 AS last_updated_by
+,        TRUNC(SYSDATE) AS last_update_date
+FROM     member m
+         INNER JOIN transaction_upload tu
+           ON     m.account_number = tu.account_number
+         INNER JOIN contact c
+           ON     m.member_id = c.member_id
+         LEFT JOIN rental r
+           ON     c.contact_id = r.customer_id
+           AND TRUNC(tu.check_out_date) = TRUNC(r.check_out_date)
+           AND TRUNC(tu.return_date) = TRUNC(r.return_date)
+         LEFT JOIN common_lookup cl1
+           ON cl1.common_lookup_table = 'RENTAL_ITEM'
+           AND cl1.common_lookup_column = 'RENTAL_ITEM_TYPE'
+           AND cl1.common_lookup_type = tu.rental_item_type
+         LEFT JOIN rental_item ri
+           ON r.rental_id = ri.rental_id
+         LEFT JOIN common_lookup cl2
+           ON cl2.common_lookup_table = 'PRICE'
+           AND cl2.common_lookup_column = 'PRICE_TYPE'
+           AND cl2.common_lookup_code = cl1.common_lookup_code
+         LEFT JOIN price p
+           ON p.item_id = tu.item_id
+           AND p.price_type = cl2.common_lookup_id
+           AND p.active_flag = 'Y'
+         LEFT JOIN common_lookup cl3
+           ON cl3.common_lookup_table = 'TRANSACTION'
+           AND cl3.common_lookup_column = 'PAYMENT_METHOD_TYPE'
+           AND cl3.common_lookup_type = tu.payment_method_type
+         LEFT JOIN common_lookup cl4
+           ON cl4.common_lookup_table = 'TRANSACTION'
+           AND cl4.common_lookup_column = 'TRANSACTION_TYPE'
+           AND cl4.common_lookup_type = tu.transaction_type
+         LEFT JOIN transaction t
+           ON t.transaction_account = tu.payment_account_number
+           AND t.rental_id = r.rental_id
+           AND t.transaction_type = cl4.common_lookup_id
+           AND t.transaction_date = tu.transaction_date
+           AND t.payment_method_type = cl3.common_lookup_id
+           AND t.payment_account_number = m.credit_card_number
+         WHERE  c.first_name = tu.first_name
+           AND    NVL(c.middle_name,'x') = NVL(tu.middle_name,'x')
+           AND    c.last_name = tu.last_name
+        GROUP BY t.transaction_id
+        ,        tu.payment_account_number
+        ,        cl4.common_lookup_id
+        ,        tu.transaction_date
+        ,        r.rental_id
+        ,        cl3.common_lookup_id
+        ,        m.credit_card_number
+        ,        1001
+        ,        TRUNC(SYSDATE)
+        ,        1001
+        ,        TRUNC(SYSDATE)) source
 ON (target.transaction_id = source.transaction_id)
 WHEN MATCHED THEN
 UPDATE SET last_updated_by = source.last_updated_by
@@ -112,17 +217,17 @@ UPDATE SET last_updated_by = source.last_updated_by
 WHEN NOT MATCHED THEN
 INSERT VALUES
 ( transaction_s1.nextval
-, source.column_name
-...
-, source.column_name);
-
-
-
-
-
-
-
-
+, source.transaction_account
+, source.transaction_type
+, source.transaction_date
+, source.transaction_amount
+, source.rental_id
+, source.payment_method_type
+, source.payment_account_number
+, source.created_by
+, source.creation_date
+, source.last_updated_by
+, source.last_update_date);
 
 
 -- Count transactions after insert
@@ -138,7 +243,7 @@ CREATE OR REPLACE PROCEDURE upload_transactions IS
 BEGIN
   -- Set save point for an all or nothing transaction.
   SAVEPOINT starting_point;
-
+/*
   -- Insert or update the table, which makes this rerunnable when the file hasn't been updated.
   MERGE INTO rental target
   USING ( ... query_statement ... ) source
@@ -219,7 +324,7 @@ BEGIN
 
   -- Save the changes.
   COMMIT;
-
+*/
 EXCEPTION
   WHEN OTHERS THEN
     ROLLBACK TO starting_point;
