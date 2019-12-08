668d95c0b87fc98ae9223c81684301c32bbdcd82
Zach Caldwell <zach.caldwell101@gmail.com>
2019-12-07 23:19:28 -0700

    [cit225] Upload lab12 logs

--- cit225/oracle/lab12/apply_oracle_lab12.sql
+++ cit225/oracle/lab12/apply_oracle_lab12.sql
@@ -19,7 +19,7 @@
 -- ------------------------------------------------------------------
 
 -- Run the prior lab script.
--- @/home/student/Data/cit225/oracle/lab11/apply_oracle_lab11.sql
+@/home/student/Data/cit225/oracle/lab11/apply_oracle_lab11.sql
 
 -- Spool log file.
 SPOOL apply_oracle_lab12.txt
