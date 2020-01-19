0e24a61b58402b7195e39d480019574b8d700cba
Zach Caldwell <zach.caldwell101@gmail.com>
2020-01-18 20:24:36 -0700

    [cit325] Patch lab2.2 and upload log

--- cit325/lab2/apply_plsql_lab2-2.sql
+++ cit325/lab2/apply_plsql_lab2-2.sql
@@ -6,8 +6,8 @@
 */
 
 -- Call seeding libraries.
--- @$LIB/cleanup_oracle.sql
--- @$LIB/Oracle12cPLSQLCode/Introduction/create_video_store.sql
+@$LIB/cleanup_oracle.sql
+@$LIB/Oracle12cPLSQLCode/Introduction/create_video_store.sql
 
 -- Open log file.
 -- SPOOL apply_plsql_lab2-2.txt
@@ -20,7 +20,7 @@ DECLARE
   lv_input     VARCHAR2(10);
 BEGIN
   lv_raw_input := '&1';
-  IF (lv_raw_input = '') THEN
+  IF (lv_raw_input IS NULL) THEN
     dbms_output.put_line('Hello World!');
   ELSIF (LENGTH(lv_raw_input) > 10) THEN
     lv_input := SUBSTR(lv_raw_input, 1, 10);
@@ -33,3 +33,5 @@ END;
 
 -- Close log file.
 -- SPOOL OFF
+
+EXIT;
