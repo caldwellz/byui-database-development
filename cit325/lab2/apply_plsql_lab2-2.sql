6e73eabc3f9b0eb6c2f7d147e1de3520c5e6132f
Zach Caldwell <zach.caldwell101@gmail.com>
2020-01-18 20:07:29 -0700

    [cit325] Add initial lab2.2 code

--- cit325/lab2/apply_plsql_lab2-2.sql
+++ cit325/lab2/apply_plsql_lab2-2.sql
@@ -1,17 +1,35 @@
 /*
 ||  Name:          apply_plsql_lab2-2.sql
-||  Date:          11 Nov 2016
+||  Author:        Zach Caldwell
+||  Date:          18 Jan 2020
 ||  Purpose:       Complete 325 Chapter 3 lab.
 */
 
 -- Call seeding libraries.
-@$LIB/cleanup_oracle.sql
-@$LIB/Oracle12cPLSQLCode/Introduction/create_video_store.sql
+-- @$LIB/cleanup_oracle.sql
+-- @$LIB/Oracle12cPLSQLCode/Introduction/create_video_store.sql
 
 -- Open log file.
-SPOOL apply_plsql_lab2-2.txt
+-- SPOOL apply_plsql_lab2-2.txt
 
--- Enter your solution here.
+SET SERVEROUTPUT ON SIZE UNLIMITED
+SET VERIFY OFF
+
+DECLARE
+  lv_raw_input CLOB;
+  lv_input     VARCHAR2(10);
+BEGIN
+  lv_raw_input := '&1';
+  IF (lv_raw_input = '') THEN
+    dbms_output.put_line('Hello World!');
+  ELSIF (LENGTH(lv_raw_input) > 10) THEN
+    lv_input := SUBSTR(lv_raw_input, 1, 10);
+    dbms_output.put_line('Hello '||lv_input||'!');
+  ELSE
+    dbms_output.put_line('Hello '||'&1'||'!');
+  END IF;
+END;
+/
 
 -- Close log file.
-SPOOL OFF
+-- SPOOL OFF
