a7bc548bc328d8df9ed3b39fc401b985f5dd4295
Zach Caldwell <zach.caldwell101@gmail.com>
2020-02-08 00:28:04 -0700

    [cit325] Add initial lab5 code

--- cit325/lab5/apply_plsql_lab5.sql
+++ cit325/lab5/apply_plsql_lab5.sql
@@ -1,18 +1,82 @@
 /*
 ||  Name:          apply_plsql_lab4.sql
-||  Date:          11 Nov 2016
+||  Author:        Zach Caldwell
 ||  Purpose:       Complete 325 Chapter 5 lab.
 */
 
 -- Call seeding libraries.
-@$LIB/cleanup_oracle.sql
-@$LIB/Oracle12cPLSQLCode/Introduction/create_video_store.sql
+-- @$LIB/cleanup_oracle.sql
+-- @$LIB/Oracle12cPLSQLCode/Introduction/create_video_store.sql
+
+SET SERVEROUTPUT ON SIZE UNLIMITED
 
 -- Open log file.
 SPOOL apply_plsql_lab5.txt
 
--- Enter your solution here.
+-- Create rating_agency sequence and table
+CREATE SEQUENCE rating_agency_s START WITH 1001;
+
+CREATE TABLE rating_agency AS
+SELECT rating_agency_s.NEXTVAL AS rating_agency_id
+,      il.item_rating AS rating
+,      il.item_rating_agency AS rating_agency
+FROM  (SELECT DISTINCT
+        i.item_rating
+      , i.item_rating_agency
+      FROM item i) il;
+
+-- Create rating_agency structure
+CREATE OR REPLACE
+  TYPE rating_agency_type IS OBJECT (
+    rating_agency_id  rating_agency.rating_agency_id%TYPE
+  , rating            rating_agency.rating%TYPE
+  , rating_agency     rating_agency.rating_agency%TYPE);
+/
+
+-- Add required column to item table
+ALTER TABLE item
+  ADD (rating_agency_id NUMBER(22));
+
+-- Begin update subroutine
+DECLARE
+  CURSOR c_ra IS
+    SELECT rating_agency_id, rating, rating_agency
+    FROM rating_agency;
+
+  TYPE rating_agency_list IS TABLE OF rating_agency_type;
+  lv_ra_list RATING_AGENCY_LIST := rating_agency_list();
+  lv_ra_obj  RATING_AGENCY_TYPE;
+BEGIN
+  -- Read the cursor into the collection
+  OPEN c_ra;
+  LOOP
+    FETCH c_ra INTO lv_ra_obj;
+    EXIT WHEN c_ra%NOTFOUND;
+
+    lv_ra_list.EXTEND;
+    lv_ra_list(lv_ra_list.COUNT) := lv_ra_obj;
+  END LOOP;
+
+  -- Loop through the collection and update the item table
+  FOR i IN 1..lv_ra_list.COUNT LOOP
+    UPDATE item
+    SET rating_agency_id = lv_ra_list(i).rating_agency_id
+    WHERE item_rating = lv_ra_list(i).rating
+      AND item_rating_agency = lv_ra_list(i).rating_agency;
+  END LOOP;
+
+  CLOSE c_ra;
+  COMMIT;
+END;
+/
+
+-- Verify results
+SELECT   rating_agency_id
+,        COUNT(*)
+FROM     item
+WHERE    rating_agency_id IS NOT NULL
+GROUP BY rating_agency_id
+ORDER BY 1;
 
 -- Close log file.
 SPOOL OFF
-
