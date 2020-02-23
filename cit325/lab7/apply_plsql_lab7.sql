f54df444e65d9e6307c82108de41ad24c6b390fc
Zach Caldwell <zach.caldwell101@gmail.com>
2020-02-22 23:18:17 -0700

    [cit325] Upload lab7 log

--- cit325/lab7/apply_plsql_lab7.sql
+++ cit325/lab7/apply_plsql_lab7.sql
@@ -5,8 +5,8 @@
 */
 
 -- Call seeding libraries.
--- @$LIB/cleanup_oracle.sql
--- @$LIB/Oracle12cPLSQLCode/Introduction/create_video_store.sql
+@$LIB/cleanup_oracle.sql
+@$LIB/Oracle12cPLSQLCode/Introduction/create_video_store.sql
 
 -- Open log file.
 SPOOL apply_plsql_lab7.txt
