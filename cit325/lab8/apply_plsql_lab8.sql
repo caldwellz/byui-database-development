6da3fe88d8515879b1afcaffa59c32c57369e1ac
Zach Caldwell <zach.caldwell101@gmail.com>
2020-02-28 22:39:42 -0700

    [cit325] Patch lab8 and upload log

--- cit325/lab8/apply_plsql_lab8.sql
+++ cit325/lab8/apply_plsql_lab8.sql
@@ -8,6 +8,9 @@
 @$LIB/cleanup_oracle.sql
 @$LIB/Oracle12cPLSQLCode/Introduction/create_video_store.sql
 
+-- Apply previous lab to get DBA updates
+@$CIT325/lab7/apply_plsql_lab7.sql
+
 -- Open log file.
 SPOOL apply_plsql_lab8.txt
 
